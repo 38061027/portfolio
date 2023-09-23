e922328c140cce5eeb286bec755ad55e9b8e2bd6
José Augusto <joseaugustogomessantos12@gmail.com>
2023-09-23 19:24:53 -0300

    carrousel criado

--- src/app/components/projetos/projetos.component.ts
+++ src/app/components/projetos/projetos.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { SharedServiceService } from 'src/app/shared-service.service';
 
-
 @Component({
   selector: 'app-projetos',
   templateUrl: './projetos.component.html',
-  styleUrls: ['./projetos.component.css']
+  styleUrls: ['./projetos.component.css'],
 })
 export class ProjetosComponent {
-
-  
+  obj = [
+    {
+      name: 'Movies Plataform',
+    },
+    {
+      name: 'Cartão de crédito',
+    },
+    {
+      name: 'Perfil fifa22',
+    },
+    {
+      name: 'Crud tour of heroes',
+    },
+  ];
 
   constructor(private sharedService: SharedServiceService) {}
 
   public funcaoCompartilhada(): void {
     this.sharedService.menuShow();
   }
-
-  
 }
