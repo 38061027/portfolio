68b21b032eaacad8bf0585f808ea737e9f1d461d
José Augusto <joseaugustogomessantos12@gmail.com>
2023-10-02 16:58:01 -0300

    Ajustando a responsividade

--- src/app/components/tecnologias/tecnologias.component.ts
+++ src/app/components/tecnologias/tecnologias.component.ts
@@ -11,10 +11,20 @@ import { SharedServiceService } from 'src/app/shared-service.service';
 
 export class TecnologiasComponent {
 
+    tecnogias: any[] = [
+  {name: 'HTML', class: 'fa-brands fa-html5'},
+  {name: 'CSS', class: 'fa-brands fa-css3-alt'},
+  {name: 'JavaScript', class: 'fa-brands fa-js'},
+  {name: 'Angular', class: 'fa-brands fa-angular'},
+  {name: 'Bootstrap', class: 'fa-brands fa-bootstrap'},
+  {name: 'SASS', class: 'fa-brands fa-sass'},
+  ]
 
   constructor(private sharedService: SharedServiceService) {}
 
   public funcaoCompartilhada(): void {
     this.sharedService.menuShow();
   }
+
+
 }
